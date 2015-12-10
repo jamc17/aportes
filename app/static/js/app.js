7f336eb471d7e92c2d5d902607a235c666368da3
José Mariñas <jamcc17@gmail.com>
2015-12-10 14:34:46 -0500

    Config view contributos in bootstrap

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -68,35 +68,4 @@
 
 	});
 
-
-
-
-	// var config = require("../../db/config/config");
-
-	// var db = new sqlite3.Database(config.dbPath);
-	 
-	// db.serialize(function() {
-	//   db.run("CREATE TABLE IF NOT EXISTS lorem (info TEXT)");
-	 
-	//   var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-	//   for (var i = 0; i < 10; i++) {
-	//       stmt.run("Ipsum " + i);
-	//   }
-	//   stmt.finalize();
-	 
-	//   db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-	//       console.log(row.id + ": " + row.info);
-	//   });
-	// });
-	 
-	// db.close();
-
-	// console.log(process.cwd());
-
-	// fs.readFile(path.join('LICENSE.md'), 'utf8', function (err, data) {
-	// 	if (err) throw err;
-
-	// 	console.log(data);
-	// });
-
 })();
